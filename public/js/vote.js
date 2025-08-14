52bf17aa96a77bbf5ee49fd95cbf800472ea93d1
SalvadorLapuz <salvadorlapuz@gmail.com>
2025-08-14 15:43:34 +0800

    Fix votes

--- public/js/vote.js
+++ public/js/vote.js
@@ -1,50 +1,40 @@
-$(document).ready(function() {
-    $('[id^="upvoteID:"]').on("click", function() {
-        const postId = this.id.split(":")[1];
-        // alert("+1 for: " + postId);
-
-        $.ajax({
-            // For MCO3, add cookies
-            data: {postID: postId},
-            method: "POST",
-            url: "/api/upvote",
-            success: function(data, textStatus, jqXHR) {
-                $(`#statusID${postId}`).text(data.numVotes);
-                if (data.userAction === "ENGAGE") {
-                    $(document.getElementById(`upvoteID:${postId}`)).addClass("upvote").removeClass("upvote-not");
-                    $(document.getElementById(`downvoteID:${postId}`)).addClass("downvote-not").removeClass("downvote");
-                } 
-                else if (data.userAction === "DISENGAGE") {
-                    $(document.getElementById(`upvoteID:${postId}`)).addClass("upvote-not").removeClass("upvote");
-                }
-            },
-            error: function() {
-                console.log("failed");
-            }
-        });
+// public/js/vote.js
+$(document).ready(function () {
+  // Upvote
+  $('[id^="upvoteID:"]').on('click', function () {
+    const postId = this.id.split(':')[1];
+    $.ajax({
+      method: 'POST',
+      url: '/api/upvote',
+      data: { postID: postId },
+      success: function (data) {
+        $(`#statusID${postId}`).text(data.numVotes);
+        if (data.userAction === 'ENGAGE' || data.userAction === 'SWITCH') {
+          $(`#upvoteID\\:${postId}`).addClass('upvote').removeClass('upvote-not');
+          $(`#downvoteID\\:${postId}`).addClass('downvote-not').removeClass('downvote');
+        } else if (data.userAction === 'DISENGAGE') {
+          $(`#upvoteID\\:${postId}`).addClass('upvote-not').removeClass('upvote');
+        }
+      }
     });
+  });
 
-    $('[id^="downvoteID:"]').on("click", function() {
-        const postId = this.id.split(":")[1];
-        // alert("-1 for: " + postId);
-
-        $.ajax({
-            data: {postID: postId},
-            method: "POST",
-            url: "/api/downvote",
-            success: function(data, textStatus, jqXHR) {
-                $(`#statusID${postId}`).text(data.numVotes);
-                if (data.userAction === "ENGAGE") {
-                    $(document.getElementById(`downvoteID:${postId}`)).addClass("downvote").removeClass("downvote-not");
-                    $(document.getElementById(`upvoteID:${postId}`)).addClass("upvote-not").removeClass("upvote");
-                } 
-                else if (data.userAction === "DISENGAGE") {
-                    $(document.getElementById(`downvoteID:${postId}`)).addClass("downvote-not").removeClass("downvote");
-                }
-            },
-            error: function() {
-                console.log("failed");
-            }
-        });
+  // Downvote
+  $('[id^="downvoteID:"]').on('click', function () {
+    const postId = this.id.split(':')[1];
+    $.ajax({
+      method: 'POST',
+      url: '/api/downvote',
+      data: { postID: postId },
+      success: function (data) {
+        $(`#statusID${postId}`).text(data.numVotes);
+        if (data.userAction === 'ENGAGE' || data.userAction === 'SWITCH') {
+          $(`#downvoteID\\:${postId}`).addClass('downvote').removeClass('downvote-not');
+          $(`#upvoteID\\:${postId}`).addClass('upvote-not').removeClass('upvote');
+        } else if (data.userAction === 'DISENGAGE') {
+          $(`#downvoteID\\:${postId}`).addClass('downvote-not').removeClass('downvote');
+        }
+      }
     });
+  });
 });
